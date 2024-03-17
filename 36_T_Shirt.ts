d4170db6aee98c976102f47161b8ba459e6c1b0d
SaadTahir1 <159918553+SaadTahir1@users.noreply.github.com>
2024-03-18 02:26:50 +0500

    Add files via upload

---
+++ 36_T_Shirt.ts
@@ -0,0 +1,23 @@
+// Write a function called make_shirt() that accepts a size and the text of a message that should be printed on the shirt. The function should print a sentence summarizing the size of the shirt and the message printed on it. Call the function.
+
+// call the function
+
+function make_shirt(size: string, text: string) {
+
+    console.log(`Creating a ${size} shirt with the message: ${text}`)
+
+}
+    
+make_shirt("large","hello world") ;
+make_shirt("medium","developer") ;
+make_shirt("small","programmer") ;
+
+
+
+
+
+
+
+
+
+// T_Shirt.js
---
+++ 37_Large_T_Shirt.ts
@@ -0,0 +1,12 @@
+// Modify the make_shirt() function so that shirts are large by default with a message that reads I love TypeScript. Make a large shirt and a medium shirt with the default message, and a shirt of any size with a different message.
+
+function make_shirt(size: string = 'Large', text: string = 'I Love TypeScript') {
+
+    console.log(`Creating a ${size} with the message ${text}`)
+}
+make_shirt();
+
+make_shirt('medium');
+
+make_shirt('small','I love you');
+
